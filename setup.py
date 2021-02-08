944694c7c7e79853d517c25db617d2c286e95f4c
Johannes Englisch <englisch@shh.mpg.de>
2021-02-08 13:37:26 +0100

    cldfbench

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+
+setup(
+    name='cldfbench_teop',
+    py_modules=['cldfbench_teop'],
+    include_package_data=True,
+    zip_safe=False,
+    entry_points={
+        'cldfbench.dataset': [
+            'teop=cldfbench_teop:Dataset',
+        ]
+    },
+    install_requires=[
+        'cldfbench',
+    ],
+    extras_require={
+        'test': [
+            'pytest-cldf',
+        ],
+    },
+)
